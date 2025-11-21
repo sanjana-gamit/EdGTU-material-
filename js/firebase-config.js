a75f7d8e74183983812bb2644abe43584e9c65e6
Sanjana Gamit <gamitsanju1430@gmail.com>
2025-11-21 13:12:45 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,13 +3,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-storage.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyCRmjcbqMRel8iQiTc3ODS6xa6Id77JwIM",
+  authDomain: "edubot-gtu.firebaseapp.com",
+  projectId: "edubot-gtu",
+  storageBucket: "edubot-gtu.firebasestorage.app",
+  messagingSenderId: "797163892030",
+  appId: "1:797163892030:web:7e6dcb3e34088df58a106f",
+  measurementId: "G-HS5LHX4EPF"
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
